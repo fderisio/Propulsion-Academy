a60aabcfec857c18f20045fc06655da5df8521d5
Fiorella De Risio <fderisio@gmail.com>
2017-05-30 11:52:52 +0200

    Spring Boot Initializr

---
+++ Week6/Day2/Twitter-Part4/src/main/java/twitter/TwitterAppApplication.java
@@ -0,0 +1,12 @@
+package twitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterAppApplication.class, args);
+	}
+}
